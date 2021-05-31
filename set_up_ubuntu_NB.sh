9c6fa4a0cb160313f037c33282c47dd6d3d0479f
butterlyn <butterlyn888@gmail.com>
2021-05-31 19:46:13 +0800

    Changed installation of pybnk python module from 'cloning then pip install folder' to 'pip install from git url'.

--- set_up_ubuntu_NB.sh
+++ set_up_ubuntu_NB.sh
@@ -26,7 +26,8 @@ git clone https://github.com/butterlyn/PYTHON_MODULE_TEMPLATE
 git clone https://github.com/butterlyn/PROJECT_FILES_BUTTERLY
 
 # ----- Install python modules -----
-python3 -m pip install numpy pandas acoular scipy jupyter Markdown pyargus twine wheel setuptools jupyterlab matplotlib wave wavefile soundfile arlpy shlnb pysoundfile ./pybnk
+python3 -m pip install numpy pandas acoular scipy jupyter Markdown pyargus twine wheel setuptools jupyterlab matplotlib wave wavefile soundfile arlpy shlnb pysoundfile
+python3 -m pip install git+https://github.com/uwasystemhealth/pybnk
 
 # ----- Misc -----
 if false; then
